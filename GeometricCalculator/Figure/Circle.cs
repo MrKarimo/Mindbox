2a9f5cebb74d07689d458643ae3d736b9555ce18
Kretov <kretov@cate.ru>
2021-12-10 09:35:14 +0300

    Добавьте файлы проекта.

---
+++ GeometricCalculator/Figure/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricCalculator.Figure
+{
+    class Circle : ISquare
+    {
+        double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalcSquare()
+        {
+            return 2 * Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+}
---
+++ GeometricCalculator/Square.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricCalculator
+{
+    public class Square
+    {
+        public double CalcSquere(ISquare figure)
+        {
+            return figure.CalcSquare();
+        }
+    }
+}
